a80ac65c81c6a8c7dcf2374c322c09f6bf2d1230
dawei <dawei.zhuang@antra.com>
2021-12-10 13:30:37 -0500

    Added: fault_tolerance demo

--- twosum_main/src/main/java/com/antra/twosum/twosum_main/service/OperationServiceImpl.java
+++ twosum_main/src/main/java/com/antra/twosum/twosum_main/service/OperationServiceImpl.java
@@ -1,10 +1,17 @@
 package com.antra.twosum.twosum_main.service;
 
+import com.antra.twosum.twosum_main.endpoint.TwoSumMainController;
 import com.antra.twosum.twosum_main.pojo.TwoSumInput;
 import com.antra.twosum.twosum_main.pojo.TwoSumResponse;
 import com.antra.twosum.twosum_main.pojo.UserInput;
 import com.antra.twosum.twosum_main.pojo.UserResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.apache.catalina.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -12,14 +19,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.CompletionStage;
+
 
 @Service
 public class OperationServiceImpl implements OperationService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(OperationServiceImpl.class);
 
 	@Autowired
 	private RestTemplate restTemplate;
 
+	private static final String BACKEND = "backend";
 	@Override
+	@Retry(name = BACKEND, fallbackMethod = "retryFallBack")
+	@RateLimiter(name = BACKEND, fallbackMethod = "rateLimiterFallBack")
+//	@CircuitBreaker(name = BACKEND, fallbackMethod = "circuitFallBack")
 	public UserResponse service(UserInput input) {
 		TwoSumInput entity = new TwoSumInput();
 		entity.setArray(input.getArray());
@@ -29,5 +43,18 @@ public class OperationServiceImpl implements OperationService {
 		res.setResults(response.getBody().getResult());
 		res.setType(input.getType());
 		return res;
+	}
+
+	private UserResponse circuitFallBack(UserInput input, Throwable e) {
+		LOGGER.error("in circuit breaker");
+		return null;
+	}
+	private UserResponse retryFallBack(UserInput input, Throwable e) {
+		LOGGER.error("in retry breaker");
+		return null;
+	}
+	private UserResponse rateLimiterFallBack(UserInput input, Throwable e) {
+		LOGGER.error("in rate breaker");
+		return null;
 	}
 }
